ccfd0164787b4163f3c6f05ffbccda21e37389ff
borisbulychev <bulychev04@edu.misis.ru>
2025-01-09 04:18:05 +0300

    Release version

---
+++ app/src/main/java/com/example/todoapp/ToDoItem.kt
@@ -0,0 +1,20 @@
+package com.example.todoapp
+
+import java.time.LocalDate
+
+enum class ImportanceField(val st:String) {
+    LOW("low"),
+    MIDDLE("mid"),
+    HIGH("high")
+}
+
+data class ToDoItem(
+    var id: String = null,
+    var text : String = "",
+    var importance: String = ImportanceField.LOW.st,
+    var deadline: String? = null,
+    var isDone : Boolean = false,
+    var creationDate : String = null,
+    var addDate : String? = null
+)
+
